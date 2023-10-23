eda877ea3a87acde1201b4a783791b8268555d5c
Aaron18s <136496132+Aaron18s@users.noreply.github.com>
2023-10-24 00:47:16 +0200

    Add files via upload

---
+++ aerosync-ios-sdk/Tests/aerosync-ios-sdkTests/aerosync_ios_sdkTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import aerosync_ios_sdk
+
+final class aerosync_ios_sdkTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
