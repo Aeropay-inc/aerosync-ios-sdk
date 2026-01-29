2fa28b17c469c924cfb751733d602a91c8e3e41b
Jayon Niravel <jayon.niravel@aeropay.com>
2026-01-29 09:33:03 -0600

    bugfix: added onBankClick event back

--- Sources/aerosync-ios-sdk/aerosync_ios_sdk.swift
+++ Sources/aerosync-ios-sdk/aerosync_ios_sdk.swift
@@ -63,6 +63,7 @@ public struct AerosyncSDK: UIViewRepresentable{
         webView.configuration.userContentController.add(coordinator, name: "onEvent")
         webView.configuration.userContentController.add(coordinator, name: "onError")
         webView.configuration.userContentController.add(coordinator, name: "onSuccess")
+        webView.configuration.userContentController.add(Coordinator(wrapper: self), name: "onBankClick")
 
         coordinator.webView = webView
 
